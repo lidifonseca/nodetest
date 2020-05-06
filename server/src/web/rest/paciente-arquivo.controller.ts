a20bd4e90028d893941d63817ca07997f4e9ee9a
Raul Cabrera Miranda <raul@qsforweb.com.br>
2020-05-06 12:48:00 -0300

    Adjustando Upload arquivos imagens

--- server/src/web/rest/paciente-arquivo.controller.ts
+++ server/src/web/rest/paciente-arquivo.controller.ts
@@ -84,10 +84,13 @@ export class PacienteArquivoController {
         .substr(2) +
       '.' +
       re.exec(arquivoFileName)[1];
+      console.info(arquivoBDName);
     await fs.mkdir('arquivos/paciente-arquivos/', { recursive: true }, err => {
       if (err) console.log(err);
       else {
         pacienteArquivo.arquivo = '/' + arquivoBDName;
+        console.info(arquivoBDName);
+        console.info(pacienteArquivo.arquivo);
         require('fs').writeFile(arquivoBDName, arquivoBase64, 'base64', function(err) {
           console.log(err);
         });
